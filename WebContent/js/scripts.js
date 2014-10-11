65b22411201de75693eafb9a981b54244a79d130
Marcel Eschmann <marcel.eschmann@gmail.com>
2014-10-11 12:46:43 +0200

    Add basic examples
    minor fix

--- WebContent/js/scripts.js
+++ WebContent/js/scripts.js
@@ -1,20 +1,54 @@
-var zmittagApp = angular.module('zmittagApp', []);
+var zmittagApp = angular.module('zmittagApp', [
+    'angular-md5',
+    'google-maps'.ns()
+]);
 
-zmittagApp.controller('mainController', function($scope) {
-    $scope.message = "Hello world";
-    $scope.destinations = [{"name": "Basilico"}, {"name": "Pickwick"}, {"name": "Thai place"}];
+zmittagApp.controller('mainController', function($scope, $http, md5) {
+    $scope.api = "http://172.27.9.66:8080/Zmittag/api/";
+
+    $scope.destinations = [];
+    $http.get($scope.api+'groups/list/').success(function(data) {
+        $scope.destinations = data;
+    });
+
     $scope.user = {
-        "name": "Max Muster",
-        "email": "max@muster.ch"
+        "name": null,
+        "email": null
     };
+
+    $scope.map = {
+        center: {
+            latitude: 45,
+            longitude: -73
+        },
+        zoom: 8
+    };
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        $scope.map.center = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+        $scope.$apply();
+    }, function() {
+        console.log('geolocation error');
+    });
+
+    $scope.test = $http.get($scope.api+'members/list/').success(function(data) {
+        $scope.user = {
+            name: data[0].name,
+            email: md5.createHash(data[0].email)
+        }
+    });
 });
 
 zmittagApp.directive('gravatar', function() {
     return {
         restrict: 'AE',
-        template: '<span class="user">' +
-                    '<img src="http://www.gravatar.com/avatar/{ md5(user.email) }?s=30" alt="{ user.name }">' +
-                '</span>',
+        template: 
+            '<span class="user">' +
+                '<img src="http://www.gravatar.com/avatar/{{ user.email }}?s=40" alt="{{ user.name }}">' +
+            '</span>',
         replace: true
     };
 });
