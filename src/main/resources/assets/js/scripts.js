3c6cae1cf44ad10ef549b8ff7deea155a97d21b1
Marcel Eschmann <marcel.eschmann@gmail.com>
2014-10-30 22:01:24 +0100

    Refactor javascript

--- src/main/resources/assets/js/scripts.js
+++ src/main/resources/assets/js/scripts.js
@@ -16,9 +16,145 @@ zmittagApp.config(function (localStorageServiceProvider) {
 zmittagApp.controller('mainController', function($scope, $log, $filter, $http, $mdToast, md5, localStorageService) {
 	
 	var hostAndPort = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '');
-    var hostAndPort = 'http://178.62.175.20:8080';
+    //var hostAndPort = 'http://178.62.175.20:8080';
+    var api = hostAndPort + "/zmittag/api/";
 
-    $scope.api = hostAndPort + "/zmittag/api/";
+    //
+    //  Tabbed Nav
+    //
+
+    $scope.nav = {
+        current: 0,
+        maxIndex: 1,
+        locked: true
+    };
+
+    //
+    //  Current User
+    //
+
+    $scope.user = {
+        "id": null,
+        "name": null,
+        "email": null,
+        "submitted": false
+    };
+
+    if(localStorageService.isSupported) {
+        $scope.user = {
+            "id": localStorageService.get('user.id'),
+            "name": localStorageService.get('user.name'),
+            "email": localStorageService.get('user.email'),
+            "submitted": localStorageService.get('user.submitted') == null ? false : true
+        };
+    }
+
+    $scope.md5 = function(value) {
+        return md5.createHash(value);
+    };
+
+    $scope.isValidUser = function() {
+        if ($scope.user.name != null && $scope.isValidEmail($scope.user.email)) { return true; };
+        return false;
+    }
+
+    $scope.isValidEmail = function(email) {
+        return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test( email ); 
+    };
+
+    // persist user info in localstorage
+    $scope.submitUserCredentials = function() {
+        if ($scope.isValidEmail) {
+            localStorageService.set('user.id', $scope.user.id);
+            localStorageService.set('user.name', $scope.user.name);
+            localStorageService.set('user.email', $scope.user.email);
+            localStorageService.set('user.submitted', true);
+            $scope.user.submitted = true;
+        };
+    }
+
+    //
+    //  Current Suggestions
+    //
+
+    $scope.destinations = [];
+    $http.get(api+'groups/list/').success(function(data) {
+        $scope.destinations = data;
+    });
+
+    // Check if current user joined a suggestion
+    $scope.hasJoined = function(id) {
+        for (var i = 0; i < $scope.destinations.length; i++) {
+            if ($scope.destinations[i].id == id) {
+                if (!$scope.destinations[i].memberList) { continue; };
+                for (var t = 0; t < $scope.destinations[i].memberList.length; t++) {
+                    if ($scope.destinations[i].memberList[t].email == $scope.user.email) {
+                        return true;
+                    };
+                };
+            };
+        };
+        return false;
+    };
+
+    // OnClick Events
+    $scope.isActiveValue = null;
+    $scope.setActive = function(id) {
+        $scope.isActiveValue = id;
+    };
+
+    $scope.isActive = function(id) {
+        return id == $scope.isActiveValue;
+    };
+
+    $scope.joinGroup = function(id) {
+        $http.post(api+'groups/'+id+'/join', {
+            member: $scope.user.name,
+            email: $scope.user.email
+        }).success(function() {
+            triggerToast('Success!', true);
+        }).error(function() {
+            triggerToast('Whoops. Something went wrong.<br>Please try again later.');
+        });
+    };
+
+    //
+    //  Recommendations
+    //
+
+    $scope.recommendations = [];
+    $http.get(api+'restaurants/bestRating').success(function(data){
+        $scope.recommendations = data;
+    });
+
+    //
+    //  Tags
+    //
+    
+    $scope.tags = [];
+    $http.get(api+'tags/list').success(function(data){
+        $scope.tags = data;
+    });
+
+    //
+    //  Toasts
+    //
+
+    function triggerToast(text, reload, duration, position) {
+        reload = typeof reload != 'undefined' ? reload : false;
+        duration = typeof duration != 'undefined' ? duration : 2000;
+        position = typeof position != 'undefined' ? position : 'top right';
+
+        $mdToast.show({
+            template: '<md-toast>'+text+'</md-toast>',
+            duration: duration,
+            position: position
+        });
+
+        if (reload) { window.setTimeout(function() { window.location.reload(false); }, 800); };
+    }
+
+    // TODO:
 
     $scope.options = {scrollwheel: false, query: "london"};
     $scope.searchbox = {
@@ -53,7 +189,7 @@ zmittagApp.controller('mainController', function($scope, $log, $filter, $http, $
     };
 
     $scope.createRestaurantAndSuggest = function(flag) {
-        $http.post($scope.api+'restaurants/add', {
+        $http.post(api+'restaurants/add', {
             name: $scope.marker.name,
             latitude: $scope.marker.coords.latitude,
             longitude: $scope.marker.coords.longitude,
@@ -62,55 +198,26 @@ zmittagApp.controller('mainController', function($scope, $log, $filter, $http, $
             tags: []
         }).success(function() {
             if (flag) {
-                $http.post($scope.api+'groups/add', {
+                $http.post(api+'groups/add', {
                     name: $scope.marker.name,
                     member: $scope.user.name,
                     email: $scope.user.email
                 }).success(function() {
-                    $mdToast.show({
-                        template: '<md-toast>Success!</md-toast>',
-                        duration: 2000,
-                        position: 'top right'
-                    });
-
-                    window.setTimeout(function() {
-                        window.location.reload(false);
-                    }, 800);
+                    triggerToast('Success!', true);
                 }).error(function() {
-                    $mdToast.show({
-                        template: '<md-toast>Whoops. Something went wrong.<br>The destination was created but you did not join.</md-toast>',
-                        duration: 2000,
-                        position: 'top right'
-                    });
-
-                    window.setTimeout(function() {
-                        window.location.reload(false);
-                    }, 800);
+                    triggerToast('Whoops. Something went wrong.<br>The destination was created but you did not join.', true);
                 });
             }else {
-                $mdToast.show({
-                    template: '<md-toast>Success!</md-toast>',
-                    duration: 2000,
-                    position: 'top right'
-                });
-
-                window.setTimeout(function() {
-                    window.location.reload(false);
-                }, 800);
+                triggerToast('Success!', true);
             }
         }).error(function() {
-            $mdToast.show({
-                template: '<md-toast>Whoops. Something went wrong.<br>Please try again later.</md-toast>',
-                duration: 2000,
-                position: 'top right'
-            });
+            triggerToast('Whoops. Something went wrong.<br>Please try again later.');
         });
     };
 
     $scope.updateMap = function(input) {
-        if (!input) {
-            return;
-        };
+        if (!input) { return; };
+
         $scope.marker = input;
         $scope.marker.id = 0;
         $scope.marker.options = {
@@ -126,95 +233,6 @@ zmittagApp.controller('mainController', function($scope, $log, $filter, $http, $
         $scope.map.center = input.coords;
     };
 
-    $scope.destinations = [];
-    $http.get($scope.api+'groups/list/').success(function(data) {
-        $scope.destinations = data;
-    });
-
-    $scope.user = {
-        "id": null,
-        "name": null,
-        "email": null,
-        "submitted": false
-    };
-
-    if(localStorageService.isSupported) {
-        $scope.user = {
-            "id": localStorageService.get('user.id'),
-            "name": localStorageService.get('user.name'),
-            "email": localStorageService.get('user.email') == null ? null : localStorageService.get('user.email'),
-            "submitted": localStorageService.get('user.submitted') == null ? false : true
-        };
-    }
-
-    $scope.data = {
-      maxIndex : 1,
-      selectedIndex : 0,
-      locked : true
-    };
-
-    $scope.isActiveValue = null;
-
-    $scope.setActive = function(id) {
-        $scope.isActiveValue = id;
-    };
-
-    $scope.isActive = function(id) {
-        return id == $scope.isActiveValue;
-    };
-
-    $scope.isValidUser = function() {
-        if ($scope.user.name != null && $scope.isValidEmail($scope.user.email)) {
-            return true;
-        };
-
-        return false;
-    }
-
-    $scope.isValidEmail = function(email) {
-        return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test( email ); 
-    };
-
-    $scope.userIsSubmitted = function() {
-        return $scope.user.submitted;
-    }
-
-    $scope.submitUserCredentials = function() {
-        if ($scope.isValidEmail) {
-            localStorageService.set('user.id', $scope.user.id);
-            localStorageService.set('user.name', $scope.user.name);
-            localStorageService.set('user.email', $scope.user.email);
-            localStorageService.set('user.submitted', true);
-            $scope.user.submitted = true;
-        };
-    }
-
-    $scope.md5 = function(value) {
-        return md5.createHash(value);
-    };
-
-    $scope.joinGroup = function(id) {
-        $http.post($scope.api+'groups/'+id+'/join', {
-            member: $scope.user.name,
-            email: $scope.user.email
-        }).success(function() {
-            $mdToast.show({
-                template: '<md-toast>Success!</md-toast>',
-                duration: 2000,
-                position: 'top right'
-            });
-            window.setTimeout(function() {
-                window.location.reload(false);
-            }, 800);
-        }).error(function() {
-            $mdToast.show({
-                template: '<md-toast>Whoops. Something went wrong.<br>Please try again later.</md-toast>',
-                duration: 2000,
-                position: 'top right'
-            });
-        });
-    };
-    
     navigator.geolocation.getCurrentPosition(function(position) {
         $scope.map.center = {
             latitude: position.coords.latitude,
@@ -225,33 +243,8 @@ zmittagApp.controller('mainController', function($scope, $log, $filter, $http, $
         //console.log('geolocation error');
     });
 
-    $scope.hasJoined = function(id) {
-        for (var i = 0; i < $scope.destinations.length; i++) {
-            if ($scope.destinations[i].id == id) {
-                if (!$scope.destinations[i].memberList) { continue; };
-                for (var t = 0; t < $scope.destinations[i].memberList.length; t++) {
-                    if ($scope.destinations[i].memberList[t].email == $scope.user.email) {
-                        return true;
-                    };
-                };
-            };
-        };
-        return false;
-    };
-    
-    $scope.recommendations = [];
-    $http.get($scope.api+'restaurants/bestRating').success(function(data){
-        $scope.recommendations = data;
-    });
-    
-    $scope.tags = [];
-    $http.get($scope.api+'tags/list').success(function(data){
-        $scope.tags = data;
-    });
-
-
     $scope.searchRestaurants = function(pattern) {
-        $http.post($scope.api+'restaurants/searchByName', {
+        $http.post(api+'restaurants/searchByName', {
             searchPattern: pattern
         }).success(function(data) {
             $scope.search.results = data;
@@ -260,17 +253,19 @@ zmittagApp.controller('mainController', function($scope, $log, $filter, $http, $
         });     
     };
 
-
-
     $scope.tagFilteredRestaurants = [];
     $scope.searchTagChanged = function(tag) {
-        $http.post($scope.api+'restaurants/ensureTag', {
+        $http.post(api+'restaurants/ensureTag', {
             searchTag: tag
         }).success(function(data) {
             $scope.tagFilteredRestaurants = data;
         });     
     };
 });
+
+//
+//  Gravatar Icon
+//
 
 zmittagApp.directive('gravatar', function() {
     return {
@@ -283,41 +278,9 @@ zmittagApp.directive('gravatar', function() {
     };
 });
 
-
-zmittagApp.directive('tfFloat', function() {
-    return {
-      restrict: 'E',
-      replace: true,
-      scope : {
-        fid : '@?',
-        value : '='
-      },
-      compile : function() {
-        return {
-          pre : function(scope, element, attrs) {
-            // transpose `disabled` flag
-            if ( angular.isDefined(attrs.disabled) ) {
-              element.attr('disabled', true);
-              scope.isDisabled = true;
-            }
-
-            // transpose the `label` value
-            scope.label = attrs.label || "";
-            scope.fid = scope.fid || scope.label;
-
-            // transpose optional `type` and `class` settings
-            element.attr('type', attrs.type || "text");
-            element.attr('class', attrs.class );
-          }
-        }
-      },
-      template:
-        '<material-input-group ng-disabled="isDisabled">' +
-          '<label for="{{fid}}">{{label}}</label>' +
-          '<material-input id="{{fid}}" ng-model="value">' +
-        '</material-input-group>'
-    };
-});
+//
+//  Google Maps Search
+//
 
 zmittagApp.directive('googleplaces', function() {
     return {
